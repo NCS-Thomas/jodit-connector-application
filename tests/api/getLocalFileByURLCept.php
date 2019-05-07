37b99dd7d81af37060d762b0b8030c7b75cf3325
thomas.vangelooven <thomas@flexmail.be>
2019-05-07 16:52:24 +0200

    Update test to get the base url based on the config

--- tests/api/getLocalFileByURLCept.php
+++ tests/api/getLocalFileByURLCept.php
@@ -3,10 +3,23 @@ $I = new ApiTester($scenario);
 
 $I->wantTo('Try get filename by URL');
 
+$I->sendGET('?action=files&source=test');
+$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
+$I->seeResponseIsJson();
+
+$I->seeResponseContainsJson([
+    "success" => true,
+    "data" => [
+        "code" => 220,
+    ]
+]);
+
+$baseUrl = $I->grabDataFromResponseByJsonPath('$.data.sources.test.baseurl');
+
 $I->sendPOST('',  [
     'action' => 'getLocalFileByUrl',
     'source' => 'test',
-    'url' => 'http://localhost:8181/files/artio.jpg'
+    'url' => $baseUrl[0].'artio.jpg'
 ]);
 
 $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
