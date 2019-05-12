1b3190ce99a122879be2c81ddb25de7841eab64f
thomas.vangelooven <thomas@flexmail.be>
2019-05-12 15:14:57 +0200

    Refactored class

--- src/Consts.php
+++ src/Consts.php
@@ -6,16 +6,18 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  * @link       https://xdsoft.net/jodit/
  */
+
 namespace Jodit;
 
-class Consts {
-	const ERROR_CODE_NOT_IMPLEMENTED = 501;
-	const ERROR_CODE_IS_NOT_WRITEBLE = 424;
-	const ERROR_CODE_NO_FILES_UPLOADED = 422;
-	const ERROR_CODE_FORBIDDEN = 403;
-	const ERROR_CODE_BAD_REQUEST = 400;
-	const ERROR_CODE_NOT_EXISTS = 404;
-	const ERROR_CODE_NOT_ACCEPTABLE = 406;
-	const ERROR_CODE_FAILED = 424;
-	const DS = DIRECTORY_SEPARATOR;
+class Consts
+{
+    public const ERROR_CODE_NOT_IMPLEMENTED = 501;
+    public const ERROR_CODE_IS_NOT_WRITEBLE = 424;
+    public const ERROR_CODE_NO_FILES_UPLOADED = 422;
+    public const ERROR_CODE_FORBIDDEN = 403;
+    public const ERROR_CODE_BAD_REQUEST = 400;
+    public const ERROR_CODE_NOT_EXISTS = 404;
+    public const ERROR_CODE_NOT_ACCEPTABLE = 406;
+    public const ERROR_CODE_FAILED = 424;
+    public const DS = DIRECTORY_SEPARATOR;
 }
