919ae4fbee4e92574d739cb3f038e1cd075783ed
thomas.vangelooven <thomas@flexmail.be>
2019-05-12 15:13:17 +0200

    Refactored function

--- src/AccessControl.php
+++ src/AccessControl.php
@@ -35,7 +35,7 @@ class AccessControl
         'IMAGE_RESIZE' => true,
         'IMAGE_CROP' => true,
     ];
-    
+
     /**
      * @param array $list
      */
@@ -62,17 +62,19 @@ class AccessControl
     }
 
     /**
-     * @param {string} $role
-     * @param {string} $action
+     * @param string $role
+     * @param string $action
+     * @param string $path
+     * @param string $fileExtension
+     * @return bool
      */
-    public function isAllow($role, $action, $path = '/', $fileExtension = '*')
+    public function isAllow(string $role, string $action, string $path = '/', string $fileExtension = '*'): bool
     {
         $action = Helper::Upperize($action);
-
         $allow = null;
 
         foreach ($this->accessList as $rule) {
-            if (!isset($rule['role']) or $rule['role'] === '*' or $rule['role'] === $role) {
+            if (!isset($rule['role']) || $rule['role'] === '*' || $rule['role'] === $role) {
 
                 if (isset($rule['path'])) {
                     if (strpos(Helper::NormalizePath($path), Helper::NormalizePath($rule['path'])) !== 0) {
